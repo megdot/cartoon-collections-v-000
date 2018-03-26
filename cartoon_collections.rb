d0cb1dcdd40f203e0c3b28196eb5a4ce4f571a9f
Megan Dotto <megan.dotto@gmail.com>
2018-03-26 18:55:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def roll_call_dwarves(dwarves)# code an argument here
-  dwarves.each_with_index {|dwarf, index| puts "#{index+1}. #{name}"}
+  dwarves.each_with_index {|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
 def summon_captain_planet# code an argument here
